3859d68a69b1dcb17e04f127d9cecda98b21d797
Patrick Wagstrom <patrick@wagstrom.net>
2021-08-24 23:07:37 -0400

    feat(meeting/images): add support for creating meetings and images
    This brings in the support for creating meetings with org-create-meeting
    and images with org-insert-image. The latter of which is bound to `C-c i
    i`.
    
    DCO-1.1-Signed-off-by: Patrick Wagstrom <patrick@wagstrom.net>

--- org-wagstrom.el
+++ org-wagstrom.el
@@ -6,6 +6,27 @@
 (require 'ido-completing-read+)
 (ido-ubiquitous-mode 1)
 
+(defvar org-directory "~/org"
+  "Default directory for org mode files")
+
+(defvar org-person-file-name "~/org/Person - %s.org"
+  "The default structure for an org file referring to a specific person")
+
+(defvar org-person-regexp "^Person - \\(.*\\).org$"
+  "Regular expression glob for finding person files")
+
+;; org mode meeting support
+(defvar org-create-meeting-list-file (concat (file-name-as-directory org-directory) "meeting_notes.org")
+  "The file that contains the list of all proevious meetings")
+
+;; paste images
+(defvar org-image-directory 
+  (file-truename
+   (file-name-as-directory
+    (concat (file-name-as-directory org-directory)
+            "images")))
+  "The directory that will store all of the images pasted into org-mode notes")
+
 (define-skeleton org-person-skeleton
   "Creates a basic skeleton for a person"
   nil
@@ -25,8 +46,21 @@
   "\n"
   )
 
-(setq-default org-directory "~/org")
-(setq-default org-person-regexp "^Person - \\(.*\\).org$")
+(define-skeleton org-meeting-skeleton
+  "Creates a basic skeleton for a meeting"
+  nil
+  "#+TITLE: " str "\n"
+  "#+STARTUP: showall\n"
+  "#+CATEGORY: meeting\n"
+  "#+TAGS: meeting\n"
+  "#+DATE: <" (format-time-string "%Y-%m-%d %a %H:%M %Z") ">\n"
+  "\n"
+  "* Attendees\n"
+  "\n"
+  "* Take Aways\n"
+  "\n"
+  "* Notes\n"
+  )
 
 
 ;; override spacebar in the mini buffer, I don't use it for completion
@@ -47,24 +81,26 @@
   (progn
     (setq rv ())
     (setq df (directory-files org-directory))
-    (dolist (elt df rv)
-      (if (string-match org-person-regexp elt)
-	  (setq rv (cons (match-string 1 elt) rv))))
-    (sort rv #'string-collate-lessp)
-    ))
+    (dolist (elem df rv)
+      (if (string-match org-person-regexp elem)
+	  (setq rv (cons (match-string 1 elem) rv))))
+    (sort rv #'string-collate-lessp)))
 
 (defun org-insert-person (person-name)
+  "Inserts a link to a person document.
+  PERSON-NAME should be the full ame of the person to create a link to.
+  If the document for PERSON-NAME does not exist then it is created."
   (interactive
    (list
     (completing-read "Person Name: " (org-get-person-files) nil nil)))
   (progn
-    (setq person-file-name (format "~/org/Person - %s.org" person-name))
+    (setq person-file-name (format org-person-file-name person-name))
     (setq person-link-text (format "[[file:%s][%s]]" person-file-name person-name))
     (setq working-buffer (current-buffer))
     (if (not (file-exists-p person-file-name))
 	(progn
 	  ;; name for new buffer. If start with space, undo is disabled
-	  (setq newBufName " xyz")
+	  (setq newBufName " org-insert-person-tempbuffer")
 
 	  ;; create a new buffer, save it to a var, so later you can switch to it or kill it
 	  (setq newBuf (generate-new-buffer newBufName))
@@ -80,3 +116,52 @@
     (set-buffer working-buffer)
     (insert person-link-text)))
 
+
+(defun org-insert-image ()
+  "Pastes an image into a file and then links the image in org-mode"
+  (interactive)
+  (setq file-name (concat (format-time-string "%Y%m%d %H%M%s") " - " (buffer-name) ".png"))
+  (setq file-name-with-path (concat org-image-directory file-name))
+  (shell-command (concat "/usr/local/bin/pngpaste \"" file-name-with-path "\"") nil nil)
+  (insert "#+CAPTION: Your_Caption_Here")
+  (newline)
+  (insert "#+ATTR_ORG: :width 500") 
+  (newline)
+  (insert (concat "[[" file-name-with-path "]]"))
+  (newline)
+  )
+
+
+ 
+;; TODO this should check to see if the meeting already exists and, if so, just open it
+(defun org-create-meeting (meeting-name)
+  (interactive "sMeeting Name:")
+  (setq meeting-name-with-date
+	(concat (format-time-string "%Y%m%d")
+		" - "
+		meeting-name))
+  (setq meeting-short-filename
+	(concat meeting-name-with-date
+		".org"))  
+  (setq filename
+	(concat
+	 (file-name-as-directory org-directory)
+	 meeting-short-filename))
+  (setq meeting-link-text (format "\n* [[file:%s][%s]]" meeting-short-filename meeting-name-with-date))
+  (message "Meeting name: %s" filename)
+
+  (setq meeting-list-buffer (get-buffer (file-name-nondirectory org-create-meeting-list-file)))
+
+  ;; add the entry to the index file
+  (if (buffer-live-p meeting-list-buffer)
+      (with-current-buffer meeting-list-buffer
+	(progn (goto-char (point-max))
+               (insert meeting-link-text)
+               (save-buffer)
+	       ))
+    (write-region meeting-link-text nil org-create-meeting-list-file 'append)
+    )
+
+  (with-current-buffer (find-file filename)
+    (org-meeting-skeleton meeting-name-with-date))
+  )
